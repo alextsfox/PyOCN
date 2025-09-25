c0ad6334acd142485d46bbcf3e64f3a4e6991a0a
Alex Fox <alextsfox@gmail.com>
2025-09-25 14:31:19 -0600

    cycle detection test passed

--- PyOCN/c_src/test.c
+++ PyOCN/c_src/test.c
@@ -24,11 +24,14 @@ int main(void){
     O-O-O-X but with 36 vertices
     */
 
+    //////////////////////////////////////////////////////////////////////////////////////
+    // X detection test
     // X shape, 5->10 and 6->9
     StreamGraph sg;
     CartPair dims = {4, 4};
     CartPair root = {3, 3};
     sg_create_empty_safe(&sg, root, dims);
+
     Vertex vertices[16] = {
         {.drained_area = 1, .adown = 4, .edges = 16, .downstream = 4, .visited = 0},
         {.drained_area = 1, .adown = 5, .edges = 16, .downstream = 4, .visited = 0},
@@ -47,7 +50,7 @@ int main(void){
         {.drained_area = 12, .adown = 15, .edges = 69, .downstream = 2, .visited = 0},
         {.drained_area = 16, .adown = 0, .edges = 65, .downstream = 255, .visited = 0}
     };
-    sg.vertices = vertices;
+    memcpy(sg.vertices, vertices, sizeof(vertices));
     sg.energy = 64.0 ;
 
     rng_seed(8472);
@@ -56,7 +59,7 @@ int main(void){
 
 
     // check that we haven't created any crosses
-    linidx_t a = 10;
+    linidx_t a = 5;
     Vertex vert = sg.vertices[a];
     clockhand_t down_new = vert.downstream;
 
@@ -78,23 +81,83 @@ int main(void){
     printf("CHECK: row %d col %d lin %d\n", check_row_col.row, check_row_col.col, sg_cart_to_lin(check_row_col, dims));
     printf("CROSS CHECK VERTEX EDGES: %u\n", cross_check_vert.edges);
 
+    bool triggered = false;
     if (down_new == 1) {
-        if (cross_check_vert.edges & (1u << 3)) printf("Trigger 1\n");  // NE flow: N vertex cannot have a SE edge
-        else printf("No trigger\n");
+        if (cross_check_vert.edges & (1u << 3)) triggered = true;  // NE flow: N vertex cannot have a SE edge
     } else if (down_new == 7) {
-        if (cross_check_vert.edges & (1u << 5)) printf("Trigger 2\n");  // NW flow: N vertex cannot have a SW edge
-        else printf("No trigger\n");
+        if (cross_check_vert.edges & (1u << 5)) triggered = true;  // NW flow: N vertex cannot have a SW edge
     } else if (down_new == 3) {
-        if (cross_check_vert.edges & (1u << 1)) printf("Trigger 3\n");  // SE flow: S vertex cannot have a NE edge
-        else printf("No trigger\n");
+        if (cross_check_vert.edges & (1u << 1)) triggered = true;  // SE flow: S vertex cannot have a NE edge
     } else if (down_new == 5) {
-        if (cross_check_vert.edges & (1u << 7)) printf("Trigger 4\n");  // SW flow: S vertex cannot have a NW edge
-        else printf("No trigger\n");
-    } else {
-        printf("No trigger\n");
+        if (cross_check_vert.edges & (1u << 7)) triggered = true;  // SW flow: S vertex cannot have a NW edge
+    }
+    assert(triggered);
+    
+    a = 6;
+    vert = sg.vertices[a];
+    down_new = vert.downstream;
+
+    check_row_col = sg_lin_to_cart(a, dims);
+    // Convert switch statements to if/else if/else structure
+    if (down_new == 1) {
+        check_row_col.row -= 1;  // NE flow: check N vertex
+    } else if (down_new == 7) {
+        check_row_col.row -= 1;  // NW flow: check N vertex
+    } else if (down_new == 3) {
+        check_row_col.row += 1;  // SE flow: check S vertex
+    } else if (down_new == 5) {
+        check_row_col.row += 1;  // SW flow: check S vertex
     }
 
-    printf("DONE\n");
+    sg_get_cart_safe(&cross_check_vert, &sg, check_row_col);
+
+    printf("CHECK: row %d col %d lin %d\n", check_row_col.row, check_row_col.col, sg_cart_to_lin(check_row_col, dims));
+    printf("CROSS CHECK VERTEX EDGES: %u\n", cross_check_vert.edges);
+
+    triggered = false;
+    if (down_new == 1) {
+        if (cross_check_vert.edges & (1u << 3)) triggered = true;  // NE flow: N vertex cannot have a SE edge
+    } else if (down_new == 7) {
+        if (cross_check_vert.edges & (1u << 5)) triggered = true;  // NW flow: N vertex cannot have a SW edge
+    } else if (down_new == 3) {
+        if (cross_check_vert.edges & (1u << 1)) triggered = true;  // SE flow: S vertex cannot have a NE edge
+    } else if (down_new == 5) {
+        if (cross_check_vert.edges & (1u << 7)) triggered = true;  // SW flow: S vertex cannot have a NW edge
+    }
+    assert(triggered);
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // Cycle dectection test
+    // contains a cycle
+    Vertex vertices_new[16] = {
+        {.drained_area = 1, .adown = 4, .edges = 16, .downstream = 4, .visited = 0},
+        {.drained_area = 1, .adown = 5, .edges = 16, .downstream = 4, .visited = 0},
+        {.drained_area = 1, .adown = 6, .edges = 16, .downstream = 4, .visited = 0},
+        {.drained_area = 1, .adown = 7, .edges = 16, .downstream = 4, .visited = 0},
+        {.drained_area = 2, .adown = 8, .edges = 17, .downstream = 4, .visited = 0},
+        {.drained_area = 2, .adown = 6, .edges = 21, .downstream = 4, .visited = 0},  // 5 goes to 6
+        {.drained_area = 2, .adown = 10, .edges = 81, .downstream = 4, .visited = 0},  // 6 goes to 10
+        {.drained_area = 2, .adown = 11, .edges = 17, .downstream = 4, .visited = 0},
+        {.drained_area = 3, .adown = 12, .edges = 17, .downstream = 4, .visited = 0},
+        {.drained_area = 3, .adown = 5, .edges = 5, .downstream = 4, .visited = 0},  // 9 goes to 5
+        {.drained_area = 3, .adown = 9, .edges = 65, .downstream = 4, .visited = 0},  // 10 goes to 9
+        {.drained_area = 3, .adown = 15, .edges = 17, .downstream = 4, .visited = 0},
+        {.drained_area = 4, .adown = 13, .edges = 5, .downstream = 2, .visited = 0},
+        {.drained_area = 8, .adown = 14, .edges = 69, .downstream = 2, .visited = 0},
+        {.drained_area = 12, .adown = 15, .edges = 69, .downstream = 2, .visited = 0},
+        {.drained_area = 16, .adown = 0, .edges = 65, .downstream = 255, .visited = 0}
+    };
+    memcpy(sg.vertices, vertices_new, sizeof(vertices_new));
+    sg.energy = 64.0;
+
+    linidx_t a_to_test[] = {1, 2, 5, 6, 9, 10};
+    for (int i = 0; i < sizeof(a_to_test) / sizeof(linidx_t); i++){
+        for (linidx_t j = 0; j < (dims.row * dims.col); j++) sg.vertices[j].visited = 0;
+        Status code = sg_flow_downstream_safe(&sg, a_to_test[i], 1);
+        assert(code == MALFORMED_GRAPH_WARNING);
+    }
+
+    sg_destroy_safe(&sg);
 
     return 0;
 }
--- sandbox.py
+++ sandbox.py
@@ -29,51 +29,9 @@ def to_c_code(ocn:PyOCN.OCN, label):
     print("sg.energy =", ocn.energy, ";")
 
 
-# ################################################
-# # basic 4x4 graph, no issues
-# label = "basic 4x4"
-# G = nx.DiGraph()
-
-# G.add_node(0, pos=(0, 0), drained_area=1)
-# G.add_node(1, pos=(0, 1), drained_area=1)
-# G.add_node(2, pos=(0, 2), drained_area=1)
-# G.add_node(3, pos=(0, 3), drained_area=1)
-# G.add_node(4, pos=(1, 0), drained_area=2)
-# G.add_node(5, pos=(1, 1), drained_area=2)
-# G.add_node(6, pos=(1, 2), drained_area=2)
-# G.add_node(7, pos=(1, 3), drained_area=2)
-# G.add_node(8, pos=(2, 0), drained_area=3)
-# G.add_node(9, pos=(2, 1), drained_area=3)
-# G.add_node(10, pos=(2, 2), drained_area=3)
-# G.add_node(11, pos=(2, 3), drained_area=3)
-# G.add_node(12, pos=(3, 0), drained_area=4)
-# G.add_node(13, pos=(3, 1), drained_area=8)
-# G.add_node(14, pos=(3, 2), drained_area=12)
-# G.add_node(15, pos=(3, 3), drained_area=16)
-
-# G.add_edge(0, 4)
-# G.add_edge(1, 5)
-# G.add_edge(2, 6)
-# G.add_edge(3, 7)
-# G.add_edge(4, 8)
-# G.add_edge(5, 9)
-# G.add_edge(6, 10)
-# G.add_edge(7, 11)
-# G.add_edge(8, 12)
-# G.add_edge(9, 13)
-# G.add_edge(10, 14)
-# G.add_edge(11, 15)
-# G.add_edge(12, 13)
-# G.add_edge(13, 14)
-# G.add_edge(14, 15)
-
-# ocn = PyOCN.OCN(init_structure=G, gamma=1, random_state=8472)
-
-# to_c_code(ocn, label)
-
-############################################
-# contains an "X"
-label = "X shape, 5->10 and 6->9"
+################################################
+# basic 4x4 graph, no issues
+label = "basic 4x4"
 G = nx.DiGraph()
 
 G.add_node(0, pos=(0, 0), drained_area=1)
@@ -98,8 +56,8 @@ G.add_edge(1, 5)
 G.add_edge(2, 6)
 G.add_edge(3, 7)
 G.add_edge(4, 8)
-G.add_edge(5, 10)
-G.add_edge(6, 9)
+G.add_edge(5, 9)
+G.add_edge(6, 10)
 G.add_edge(7, 11)
 G.add_edge(8, 12)
 G.add_edge(9, 13)
@@ -110,9 +68,51 @@ G.add_edge(13, 14)
 G.add_edge(14, 15)
 
 ocn = PyOCN.OCN(init_structure=G, gamma=1, random_state=8472)
-print(ocn.sg._vertices.flatten()[9])
 
 to_c_code(ocn, label)
+
+# ############################################
+# # contains an "X"
+# label = "X shape, 5->10 and 6->9"
+# G = nx.DiGraph()
+
+# G.add_node(0, pos=(0, 0), drained_area=1)
+# G.add_node(1, pos=(0, 1), drained_area=1)
+# G.add_node(2, pos=(0, 2), drained_area=1)
+# G.add_node(3, pos=(0, 3), drained_area=1)
+# G.add_node(4, pos=(1, 0), drained_area=2)
+# G.add_node(5, pos=(1, 1), drained_area=2)
+# G.add_node(6, pos=(1, 2), drained_area=2)
+# G.add_node(7, pos=(1, 3), drained_area=2)
+# G.add_node(8, pos=(2, 0), drained_area=3)
+# G.add_node(9, pos=(2, 1), drained_area=3)
+# G.add_node(10, pos=(2, 2), drained_area=3)
+# G.add_node(11, pos=(2, 3), drained_area=3)
+# G.add_node(12, pos=(3, 0), drained_area=4)
+# G.add_node(13, pos=(3, 1), drained_area=8)
+# G.add_node(14, pos=(3, 2), drained_area=12)
+# G.add_node(15, pos=(3, 3), drained_area=16)
+
+# G.add_edge(0, 4)
+# G.add_edge(1, 5)
+# G.add_edge(2, 6)
+# G.add_edge(3, 7)
+# G.add_edge(4, 8)
+# G.add_edge(5, 10)
+# G.add_edge(6, 9)
+# G.add_edge(7, 11)
+# G.add_edge(8, 12)
+# G.add_edge(9, 13)
+# G.add_edge(10, 14)
+# G.add_edge(11, 15)
+# G.add_edge(12, 13)
+# G.add_edge(13, 14)
+# G.add_edge(14, 15)
+
+# ocn = PyOCN.OCN(init_structure=G, gamma=1, random_state=8472)
+# print(ocn.sg._vertices.flatten()[9])
+
+# to_c_code(ocn, label)
 
 
 # ############################################
@@ -154,33 +154,33 @@ to_c_code(ocn, label)
 
 # PyOCN.plot_positional_digraph(G)
 
-# str = """
-# // contains a cycle
-# StreamGraph sg;
-# CartPair dims = {4, 4};
-# CartPair root = {3, 3};
-# sg_create_empty_safe(&sg, root, dims);
-# Vertex vertices[16] = {
-#     {.drained_area = 1, .adown = 4, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 1, .adown = 5, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 1, .adown = 6, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 1, .adown = 7, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 2, .adown = 8, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 2, .adown = 6, .edges = 21, .downstream = 4, .visited = 0},  // 5 goes to 6
-#     {.drained_area = 2, .adown = 10, .edges = 81, .downstream = 4, .visited = 0},  // 6 goes to 10
-#     {.drained_area = 2, .adown = 11, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 3, .adown = 12, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 3, .adown = 5, .edges = 5, .downstream = 4, .visited = 0},  // 9 goes to 5
-#     {.drained_area = 3, .adown = 9, .edges = 65, .downstream = 4, .visited = 0},  // 10 goes to 9
-#     {.drained_area = 3, .adown = 15, .edges = 16, .downstream = 4, .visited = 0},
-#     {.drained_area = 4, .adown = 13, .edges = 4, .downstream = 2, .visited = 0},
-#     {.drained_area = 8, .adown = 14, .edges = 4, .downstream = 2, .visited = 0},
-#     {.drained_area = 12, .adown = 15, .edges = 4, .downstream = 2, .visited = 0},
-#     {.drained_area = 16, .adown = 0, .edges = 0, .downstream = 255, .visited = 0}
-# };
-# sg.vertices = vertices;
-# sg.energy = 64.0 ;
-# """
+"""
+// contains a cycle
+StreamGraph sg;
+CartPair dims = {4, 4};
+CartPair root = {3, 3};
+sg_create_empty_safe(&sg, root, dims);
+Vertex vertices[16] = {
+    {.drained_area = 1, .adown = 4, .edges = 16, .downstream = 4, .visited = 0},
+    {.drained_area = 1, .adown = 5, .edges = 16, .downstream = 4, .visited = 0},
+    {.drained_area = 1, .adown = 6, .edges = 16, .downstream = 4, .visited = 0},
+    {.drained_area = 1, .adown = 7, .edges = 16, .downstream = 4, .visited = 0},
+    {.drained_area = 2, .adown = 8, .edges = 17, .downstream = 4, .visited = 0},
+    {.drained_area = 2, .adown = 6, .edges = 21, .downstream = 4, .visited = 0},  // 5 goes to 6
+    {.drained_area = 2, .adown = 10, .edges = 81, .downstream = 4, .visited = 0},  // 6 goes to 10
+    {.drained_area = 2, .adown = 11, .edges = 17, .downstream = 4, .visited = 0},
+    {.drained_area = 3, .adown = 12, .edges = 17, .downstream = 4, .visited = 0},
+    {.drained_area = 3, .adown = 5, .edges = 5, .downstream = 4, .visited = 0},  // 9 goes to 5
+    {.drained_area = 3, .adown = 9, .edges = 65, .downstream = 4, .visited = 0},  // 10 goes to 9
+    {.drained_area = 3, .adown = 15, .edges = 17, .downstream = 4, .visited = 0},
+    {.drained_area = 4, .adown = 13, .edges = 5, .downstream = 2, .visited = 0},
+    {.drained_area = 8, .adown = 14, .edges = 69, .downstream = 2, .visited = 0},
+    {.drained_area = 12, .adown = 15, .edges = 69, .downstream = 2, .visited = 0},
+    {.drained_area = 16, .adown = 0, .edges = 65, .downstream = 255, .visited = 0}
+};
+sg.vertices = vertices;
+sg.energy = 64.0 ;
+"""
 
 # ################################
 # # contains staring contest
