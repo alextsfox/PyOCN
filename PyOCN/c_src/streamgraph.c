36f11cc7ce131639dce73996aaba163953bcfcf2
Alex Fox <alextsfox@gmail.com>
2025-09-27 00:33:45 -0600

    reverting to RMO

--- PyOCN/c_src/streamgraph.c
+++ PyOCN/c_src/streamgraph.c
@@ -44,10 +44,7 @@ const OffsetPair offsets[8] = {
     {-1, -1}  // NW
 };
 
-// 2d raster is block-tiled in memory to improve cache locality
-/*
- * Converts cartesian (row, col) to linear index in block-tiled memory
- */
+// // 2d raster is block-tiled in memory to improve cache locality. Turns out this doesn't make a big difference.
 // const cartidx_t TILE_SIZE = 2;
 // linidx_t sg_cart_to_lin(CartPair coords, CartPair dims){
 //     cartidx_t row = coords.row;
@@ -60,14 +57,6 @@ const OffsetPair offsets[8] = {
 //     linidx_t a = (TILE_SIZE * TILE_SIZE * k) + (TILE_SIZE * r_divT.rem) + c_divT.rem;
 //     return a;
 // }
-// for now, we just use normal row-major order until we finish debugging the main algorithm
-linidx_t sg_cart_to_lin(CartPair coords, CartPair dims){
-    return (coords.row * dims.col + coords.col);
-}
-CartPair sg_lin_to_cart(linidx_t a, CartPair dims){
-    div_t adiv = div(a, dims.col);
-    return (CartPair){adiv.quot, adiv.rem};
-}
 // CartPair sg_lin_to_cart(linidx_t a, CartPair dims){
 //     cartidx_t p = a % TILE_SIZE;
 //     cartidx_t q = (a / TILE_SIZE) % TILE_SIZE;
@@ -77,6 +66,15 @@ CartPair sg_lin_to_cart(linidx_t a, CartPair dims){
 //     cartidx_t i = q + 2 * (k / (dims.col / TILE_SIZE));
 //     return (CartPair){i, j};
 // }
+
+// for now, we just use normal row-major order until we finish debugging the main algorithm
+linidx_t sg_cart_to_lin(CartPair coords, CartPair dims){
+    return (coords.row * dims.col + coords.col);
+}
+CartPair sg_lin_to_cart(linidx_t a, CartPair dims){
+    div_t adiv = div(a, dims.col);
+    return (CartPair){adiv.quot, adiv.rem};
+}
 Status sg_clockhand_to_lin_safe(linidx_t *a_down, linidx_t a, clockhand_t down, CartPair dims){
     CartPair row_col = sg_lin_to_cart(a, dims);
     OffsetPair offset = offsets[down];
