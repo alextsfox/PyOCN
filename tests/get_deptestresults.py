fff22e21336f9e5a2af074f0ab9885a78fe91132
Alex Fox <alextsfox@gmail.com>
2025-10-01 10:07:39 -0600

    update dependencies

--- tests/get_deptestresults.py
+++ tests/get_deptestresults.py
@@ -20,27 +20,28 @@ if __name__ == "__main__":
         for n, v in deps.items():
             results[n].append(float(v.strip(".*")))
     successes = pd.DataFrame(results).query("success").drop(columns=["success"])
-    # find the pareto-minimal combos of dependency versions for each python version
-    minimal_combos = []
-    for py_ver, group in successes.groupby("python_ver"):
-        group = group.reset_index(drop=True)
-        dominated = set()
-        for i, row_i in group.iterrows():
-            for j, row_j in group.iterrows():
-                if i == j or j in dominated:
-                    continue
-                le_all = True
-                lt_any = False
-                for dep in depnames:
-                    vi = row_i[dep]
-                    vj = row_j[dep]
-                    if vi > vj:
-                        le_all = False
-                        break
-                    if vi < vj:
-                        lt_any = True
-                if le_all and lt_any:
-                    dominated.add(i)
-                    break
-        minimal_combos.extend(group.drop(index=dominated).to_dict(orient="records"))
-    pd.DataFrame(minimal_combos).to_csv("/Users/alex/Documents/pet-projects/lang-learn/C/OCN/logs/dep_test_results.csv", index=False)
+    # # find the pareto-minimal combos of dependency versions for each python version
+    # minimal_combos = []
+    # for py_ver, group in successes.groupby("python_ver"):
+    #     group = group.reset_index(drop=True)
+    #     dominated = set()
+    #     for i, row_i in group.iterrows():
+    #         for j, row_j in group.iterrows():
+    #             if i == j or j in dominated:
+    #                 continue
+    #             le_all = True
+    #             lt_any = False
+    #             for dep in depnames:
+    #                 vi = row_i[dep]
+    #                 vj = row_j[dep]
+    #                 if vi > vj:
+    #                     le_all = False
+    #                     break
+    #                 if vi < vj:
+    #                     lt_any = True
+    #             if le_all and lt_any:
+    #                 dominated.add(i)
+    #                 break
+    #     minimal_combos.extend(group.drop(index=dominated).to_dict(orient="records"))
+    # pd.DataFrame(minimal_combos).to_csv("/Users/alex/Documents/pet-projects/lang-learn/C/OCN/logs/dep_test_results.csv", index=False)
+    successes.to_csv("/Users/alex/Documents/pet-projects/lang-learn/C/OCN/logs/dep_test_results.csv", index=False)
