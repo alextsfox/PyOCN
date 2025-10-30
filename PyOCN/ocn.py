efdaa5be4508957f1e48828405cc1327654d708f
Alex Fox <alextsfox@gmail.com>
2025-10-30 08:29:06 -0600

    temporarily commenting out graph traversal methods

--- PyOCN/ocn.py
+++ PyOCN/ocn.py
@@ -379,114 +379,115 @@ class OCN:
     ###########################
     #    GRAPH TRAVERSAL      #
     ###########################
-    def predecessors(self, pos:tuple[int, int]) -> Generator[tuple[int, int], None, None]:
-        """Returns an iterator over predecessor nodes of the node at position `pos` in the OCN.
-        A predecessor is defined as an immediate upstream neighbor.
+    # # Commenting out for now. May decide to re-introduce later
+    # def predecessors(self, pos:tuple[int, int]) -> Generator[tuple[int, int], None, None]:
+    #     """Returns an iterator over predecessor nodes of the node at position `pos` in the OCN.
+    #     A predecessor is defined as an immediate upstream neighbor.
 
-        A predecessor of a node n is a node m such that there exists a directed edge from m to n.
+    #     A predecessor of a node n is a node m such that there exists a directed edge from m to n.
 
-        Mirrors the behavior of `networkx.DiGraph.predecessors`, but works directly on the OCN C graph structure.
+    #     Mirrors the behavior of `networkx.DiGraph.predecessors`, but works directly on the OCN C graph structure.
 
-        Yields (row, col) of successors.
+    #     Yields (row, col) of successors.
 
-        Parameters
-        ----------
-        ocn : OCN
-            The OCN instance.
-        pos : tuple[int, int]
-            The (row, col) position of the node whose predecessors are to be found.
+    #     Parameters
+    #     ----------
+    #     ocn : OCN
+    #         The OCN instance.
+    #     pos : tuple[int, int]
+    #         The (row, col) position of the node whose predecessors are to be found.
         
-        Raises
-        -------
-        TypeError
-            If `pos` is not a tuple of two integers.
-        IndexError
-            If `pos` is out of bounds for the current OCN grid.
+    #     Raises
+    #     -------
+    #     TypeError
+    #         If `pos` is not a tuple of two integers.
+    #     IndexError
+    #         If `pos` is out of bounds for the current OCN grid.
 
-        See Also
-        --------
-        :meth:`OCN.successors`
-        """
+    #     See Also
+    #     --------
+    #     :meth:`OCN.successors`
+    #     """
 
-        pos = tuple(pos)
-        if len(pos) != 2 or not all(isinstance(p, int) for p in pos):
-            raise TypeError(f"Position must be a tuple of two integers. Got {pos}.")
-        if (pos[0] < 0 or pos[0] >= self.dims[0]) or (pos[1] < 0 or pos[1] >= self.dims[1]):
-            raise IndexError(f"Position {pos} is out of bounds for OCN with dimensions {self.dims}.")
+    #     pos = tuple(pos)
+    #     if len(pos) != 2 or not all(isinstance(p, int) for p in pos):
+    #         raise TypeError(f"Position must be a tuple of two integers. Got {pos}.")
+    #     if (pos[0] < 0 or pos[0] >= self.dims[0]) or (pos[1] < 0 or pos[1] >= self.dims[1]):
+    #         raise IndexError(f"Position {pos} is out of bounds for OCN with dimensions {self.dims}.")
 
-        # convert (row, col) to linear index
-        a = _bindings.libocn.fg_cart_to_lin(
-            _bindings.CartPair_C(row=pos[0], col=pos[1]),
-            _bindings.CartPair_C(row=self.dims[0], col=self.dims[1]),
-        )
+    #     # convert (row, col) to linear index
+    #     a = _bindings.libocn.fg_cart_to_lin(
+    #         _bindings.CartPair_C(row=pos[0], col=pos[1]),
+    #         _bindings.CartPair_C(row=self.dims[0], col=self.dims[1]),
+    #     )
 
-        upstream_indices = (_bindings.linidx_t * 8)()
-        nupstream = _bindings.linidx_t()
-        check_status(_bindings.libocn.fg_find_upstream_neighbors(
-            upstream_indices, 
-            ctypes.byref(nupstream),
-            self.__p_c_graph, 
-            _bindings.linidx_t(a)
-        ))
-        count = int(nupstream.value)
+    #     upstream_indices = (_bindings.linidx_t * 8)()
+    #     nupstream = _bindings.linidx_t()
+    #     check_status(_bindings.libocn.fg_find_upstream_neighbors(
+    #         upstream_indices, 
+    #         ctypes.byref(nupstream),
+    #         self.__p_c_graph, 
+    #         _bindings.linidx_t(a)
+    #     ))
+    #     count = int(nupstream.value)
 
-        # convert back to cartesian (row, col)
-        for idx in upstream_indices[:count]:
-            cart = _bindings.libocn.fg_lin_to_cart(
-                idx, 
-                _bindings.CartPair_C(row=self.dims[0], col=self.dims[1])
-            )
-            yield (int(cart.row), int(cart.col))
+    #     # convert back to cartesian (row, col)
+    #     for idx in upstream_indices[:count]:
+    #         cart = _bindings.libocn.fg_lin_to_cart(
+    #             idx, 
+    #             _bindings.CartPair_C(row=self.dims[0], col=self.dims[1])
+    #         )
+    #         yield (int(cart.row), int(cart.col))
 
 
-    def successors(self, pos: tuple[int, int]) -> Generator[tuple[int, int], None, None]:
-        """Returns an iterator over successor nodes of the node at position `pos` in the OCN.
-        A successor is the immediate downstream neighbor (at most one; none if root).
+    # def successors(self, pos: tuple[int, int]) -> Generator[tuple[int, int], None, None]:
+    #     """Returns an iterator over successor nodes of the node at position `pos` in the OCN.
+    #     A successor is the immediate downstream neighbor (at most one; none if root).
 
-        Mirrors the behavior of `networkx.DiGraph.successors`, but works directly on the OCN C graph structure.
-        Yields (row, col) of successors.
+    #     Mirrors the behavior of `networkx.DiGraph.successors`, but works directly on the OCN C graph structure.
+    #     Yields (row, col) of successors.
 
-        Parameters
-        ----------
-        pos : tuple[int, int]
-            The (row, col) position of the node whose successors are to be found.
+    #     Parameters
+    #     ----------
+    #     pos : tuple[int, int]
+    #         The (row, col) position of the node whose successors are to be found.
         
-        Raises
-        -------
-        TypeError
-            If `pos` is not a tuple of two integers.
-        IndexError
-            If `pos` is out of bounds for the current OCN grid.
-        """
-        pos = tuple(pos)
-        if len(pos) != 2 or not all(isinstance(p, int) for p in pos):
-            raise TypeError(f"Position must be a tuple of two integers. Got {pos}.")
-        if (pos[0] < 0 or pos[0] >= self.dims[0]) or (pos[1] < 0 or pos[1] >= self.dims[1]):
-            raise IndexError(f"Position {pos} is out of bounds for OCN with dimensions {self.dims}.")
+    #     Raises
+    #     -------
+    #     TypeError
+    #         If `pos` is not a tuple of two integers.
+    #     IndexError
+    #         If `pos` is out of bounds for the current OCN grid.
+    #     """
+    #     pos = tuple(pos)
+    #     if len(pos) != 2 or not all(isinstance(p, int) for p in pos):
+    #         raise TypeError(f"Position must be a tuple of two integers. Got {pos}.")
+    #     if (pos[0] < 0 or pos[0] >= self.dims[0]) or (pos[1] < 0 or pos[1] >= self.dims[1]):
+    #         raise IndexError(f"Position {pos} is out of bounds for OCN with dimensions {self.dims}.")
 
-        # (row, col) -> linear index
-        a = _bindings.libocn.fg_cart_to_lin(
-            _bindings.CartPair_C(row=pos[0], col=pos[1]),
-            _bindings.CartPair_C(row=self.dims[0], col=self.dims[1]),
-        )
+    #     # (row, col) -> linear index
+    #     a = _bindings.libocn.fg_cart_to_lin(
+    #         _bindings.CartPair_C(row=pos[0], col=pos[1]),
+    #         _bindings.CartPair_C(row=self.dims[0], col=self.dims[1]),
+    #     )
 
-        # Read vertex to get downstream direction
-        vert = _bindings.Vertex_C()
-        check_status(_bindings.libocn.fg_get_lin(
-            ctypes.byref(vert),
-            self.__p_c_graph,
-            _bindings.linidx_t(a),
-        ))
+    #     # Read vertex to get downstream direction
+    #     vert = _bindings.Vertex_C()
+    #     check_status(_bindings.libocn.fg_get_lin(
+    #         ctypes.byref(vert),
+    #         self.__p_c_graph,
+    #         _bindings.linidx_t(a),
+    #     ))
 
-        # If root, no successors
-        if int(vert.downstream) == _bindings.IS_ROOT:
-            return
+    #     # If root, no successors
+    #     if int(vert.downstream) == _bindings.IS_ROOT:
+    #         return
         
-        cart = _bindings.libocn.fg_lin_to_cart(
-            vert.adown, 
-            _bindings.CartPair_C(row=self.dims[0], col=self.dims[1])
-        )
-        yield (int(cart.row), int(cart.col))        
+    #     cart = _bindings.libocn.fg_lin_to_cart(
+    #         vert.adown, 
+    #         _bindings.CartPair_C(row=self.dims[0], col=self.dims[1])
+    #     )
+    #     yield (int(cart.row), int(cart.col))        
 
     ###########################
     #    EXPORT               #
